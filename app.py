256720690cf51a903c85459e2a06ea72d347b2db
Akshaya Sivagami <akshayasivagami3@gmail.com>
2026-03-09 22:27:32 +0530

    Refactor data processing for clarity

--- app.py
+++ app.py
@@ -4,15 +4,11 @@ import streamlit as st
 data = pd.read_csv("dataset/
 hospital_billing.csv")
 
-missing_claims =
-data[data["Claim_Submitted"] == "No"]
+missing_claims = data[data["Claim_Submitted"] == "No"]
 
-data["Underpaid"] = 
-data["Expected_Amount"] - 
-data["Paid_Amount"]
+data["Underpaid"] = data["Expected_Amount"] - data["Paid_Amount"]
 
-underpaid_claims = data[data["Underpaid"]
-> 0]
+underpaid_claims = data[data["Underpaid"] > 0]
 
 st.title("AI Revenue Leakage Detection System")
 
